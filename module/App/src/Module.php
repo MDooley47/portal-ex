a964ece081be1389f74bd6f90bbe6aac04237d7f
Matthew Dooley <Matthew@Dooley.xyz>
2017-11-21 16:27:21 -0600

    Fixes typeos

--- module/App/src/Module.php
+++ module/App/src/Module.php
@@ -9,9 +9,9 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
 class Module implements ConfigProviderInterface
 {
-    public getConfig()
+    public function getConfig()
     {
-        return include __DIR__ '/../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getServiceConfig()
