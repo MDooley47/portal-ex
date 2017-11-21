34329168caee11747c14f2323e075d6588029d89
Matthew Dooley <Matthew@Dooley.xyz>
2017-11-21 16:19:12 -0600

    Adds portal database and app table

---
+++ data/load_db.php
@@ -0,0 +1,19 @@
+<?php
+
+try
+{
+    $db = new PDO('pgsql:host=db;user=postgres;password=asdfgh');
+
+    $db->exec("CREATE DATABASE `portal`;");
+
+    $fh = fopen(__DIR__ . '/schema.sql', 'r');
+    while ($line = fread($fh, 4096))
+    {
+        $db->exec($line);
+    }
+    fclose($fh);
+}
+catch (PDOException $e)
+{
+    die("DB ERROR: ". $e->getMessage() . "\n");
+}
---
+++ data/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE app (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255) NOT NULL, url varchar(255) NOT NULL, iconPath varchar(255) NOT NULL);
