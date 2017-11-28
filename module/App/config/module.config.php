4f84f803653d5a730959297cdfad41c17ff27b98
Matthew Dooley <Matthew@Dooley.xyz>
2017-11-28 16:08:10 -0600

    Fixes router array

--- module/App/config/module.config.php
+++ module/App/config/module.config.php
@@ -2,21 +2,29 @@
 
 namespace App;
 
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
 return [
     'router' => [
-        'app' => [
-            'type' => Segment::class,
-            'options' => '/app[/:action[/:id]]',
-            'constraints' => [
-                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                'id' => '[0-9]+'
-            ],
-            'defaults' => [
-                'controller' => Controller\AppController::class,
-                'action' => 'index',
+        'routes' => [
+            'app' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/app[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AppController::class,
+                        'action'     => 'index',
+                    ],
+                ],
             ],
         ],
     ],
+
 
     'view_manager' => [
         'template_path_stack' => [
