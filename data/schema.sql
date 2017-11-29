e7e21921c7276f2ded20054184cc8e6602075a90
Matthew Dooley <Matthew@Dooley.xyz>
2017-11-28 18:57:14 -0600

    Updates SQL schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,1 +1,8 @@
-CREATE TABLE app (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255) NOT NULL, url varchar(255) NOT NULL, iconPath varchar(255) NOT NULL);
+CREATE TABLE app (id SERIAL PRIMARY KEY, name varchar(255) NOT NULL, url varchar(255) NOT NULL, iconPath varchar(255) NOT NULL);
+
+
+INSERT INTO app (name, url, iconPath) VALUES ('The Military Wives', 'In My Dreams', '/volumes/storage/appIcons/');
+INSERT INTO app (name, url, iconPath) VALUES ('Adele', '21', '/volumes/storage/appIcons/');
+INSERT INTO app (name, url, iconPath) VALUES ('Bruce Springsteen', 'Wrecking Ball (Deluxe)', '/volumes/storage/appIcons/');
+INSERT INTO app (name, url, iconPath) VALUES ('Lana Del Rey', 'Born To Die', '/volumes/storage/appIcons/');
+INSERT INTO app (name, url, iconPath) VALUES ('Gotye', 'Making Mirrors', '/volumes/storage/appIcons/');
