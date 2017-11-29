e01c89ebff64d23ca263d8099fab8efe87dfea0e
Matthew Dooley <Matthew@Dooley.xyz>
2017-11-28 18:57:45 -0600

    Removes ``

--- data/load_db.php
+++ data/load_db.php
@@ -4,7 +4,7 @@ try
 {
     $db = new PDO('pgsql:host=db;user=postgres;password=asdfgh');
 
-    $db->exec("CREATE DATABASE `portal`;");
+    $db->exec("CREATE DATABASE portal;");
 
     $fh = fopen(__DIR__ . '/schema.sql', 'r');
     while ($line = fread($fh, 4096))
