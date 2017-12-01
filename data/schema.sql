1217631ca7754a7938701e745e9d86c3db09224f
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-01 16:48:31 -0600

    Fixes SQl and adds releated example content

--- data/schema.sql
+++ data/schema.sql
@@ -1,8 +1,9 @@
-CREATE TABLE app (id SERIAL PRIMARY KEY, name varchar(255) NOT NULL, url varchar(255) NOT NULL, iconPath varchar(255) NOT NULL);
+CREATE TABLE app (id SERIAL PRIMARY KEY, name varchar(255) NOT NULL, url varchar(255) NOT NULL, "iconPath" varchar(255) NOT NULL);
 
 
-INSERT INTO app (name, url, iconPath) VALUES ('The Military Wives', 'In My Dreams', '/volumes/storage/appIcons/');
-INSERT INTO app (name, url, iconPath) VALUES ('Adele', '21', '/volumes/storage/appIcons/');
-INSERT INTO app (name, url, iconPath) VALUES ('Bruce Springsteen', 'Wrecking Ball (Deluxe)', '/volumes/storage/appIcons/');
-INSERT INTO app (name, url, iconPath) VALUES ('Lana Del Rey', 'Born To Die', '/volumes/storage/appIcons/');
-INSERT INTO app (name, url, iconPath) VALUES ('Gotye', 'Making Mirrors', '/volumes/storage/appIcons/');
+INSERT INTO app (name, url, "iconPath") VALUES ('Google Drive', 'https://accounts.google.com/ServiceLogin?service=wise&ltmpl=drive', '/volumes/storage/images/google_drive.png');
+INSERT INTO app (name, url, "iconPath") VALUES ('Google Docs', 'https://docs.google.com', '/volumes/storage/images/google_docs.png');
+INSERT INTO app (name, url, "iconPath") VALUES ('Digital Ocean', 'https://digitalocean.com', '/volumes/storage/images/digital_ocean.png');
+INSERT INTO app (name, url, "iconPath") VALUES ('Gmail', 'https://gmail.com', '/volumes/storage/images/gmail.png');
+INSERT INTO app (name, url, "iconPath") VALUES ('UNOmaha', 'https://unomaha.edu', '/volumes/storage/images/unomaha.png');
+INSERT INTO app (name, url, "iconPath") VALUES ('Ubuntu', 'https://ubuntu.com', '/volumes/storage/images/ubuntu.png');
