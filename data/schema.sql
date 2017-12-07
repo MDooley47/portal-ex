4f8be5912526183894cea032a21ebacda42bb13d
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-07 16:11:58 -0600

    Corrects file type

--- data/schema.sql
+++ data/schema.sql
@@ -5,5 +5,5 @@ INSERT INTO app (name, url, "iconPath") VALUES ('Google Drive', 'https://account
 INSERT INTO app (name, url, "iconPath") VALUES ('Google Docs', 'https://docs.google.com', '/volumes/storage/images/google_docs.png');
 INSERT INTO app (name, url, "iconPath") VALUES ('Digital Ocean', 'https://digitalocean.com', '/volumes/storage/images/digital_ocean.png');
 INSERT INTO app (name, url, "iconPath") VALUES ('Gmail', 'https://gmail.com', '/volumes/storage/images/gmail.png');
-INSERT INTO app (name, url, "iconPath") VALUES ('UNOmaha', 'https://unomaha.edu', '/volumes/storage/images/unomaha.png');
+INSERT INTO app (name, url, "iconPath") VALUES ('UNOmaha', 'https://unomaha.edu', '/volumes/storage/images/unomaha.jpg');
 INSERT INTO app (name, url, "iconPath") VALUES ('Ubuntu', 'https://ubuntu.com', '/volumes/storage/images/ubuntu.png');
