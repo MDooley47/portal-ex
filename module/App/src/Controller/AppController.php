f4f8d200de5e2951c1afc20a7c2f632a3e5e1349
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-08 17:08:43 -0600

    [WIP] adds addAction

--- module/App/src/Controller/AppController.php
+++ module/App/src/Controller/AppController.php
@@ -27,7 +27,50 @@ class AppController extends AbstractActionController
 
     public function addAction()
     {
+        $form = new AppForm('app');
 
+        $request = $this->getRequest();
+
+        if ($request->isPost())
+        {
+            $post = array_merge_recursive(
+                $request->getPost()->toArray(),
+                $request->getFiles()->toArray()
+            );
+
+            $app = new App();
+
+            $form->setInputFilter($app->getInputFilter(true));
+            $form->setData($post);
+
+            if ($form->isValid())
+            {
+                $data = $form->getData();
+
+                $newName = "/volumes/storage/images/" .
+                    pathinfo($data['icon']['tmp_name'], PATHINFO_BASENAME) .
+                    "." .
+                    pathinfo($data['icon']['name'], PATHINFO_EXTENSION);
+
+                rename($data['icon']['tmp_name'], $newName);
+
+                $data['iconPath'] = $newName;
+                $form->setData($data);
+
+            }
+
+            $form->setInputFilter($app->getInputFilter(true));
+            if ($form->isValid())
+            {
+                $app->exchangeArray($form->getData());
+                $this->table->saveApp($app);
+
+                return $this->redirect()->toRoute('app', ['action' => 'add']);
+            }
+
+        }
+
+        return ['form' => $form];
     }
 
     public function editAction()
