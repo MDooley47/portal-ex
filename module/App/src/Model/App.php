db6c3e18b569b436f17bcb6c31d8e4c6cdea093a
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-08 18:27:24 -0600

    Adds getInputFilter

--- module/App/src/Model/App.php
+++ module/App/src/Model/App.php
@@ -2,12 +2,18 @@
 
 namespace App\Model;
 
+use App\InputFilter\AppNameFilter;
+use App\InputFilter\AppURLFilter;
+use App\InputFilter\AppIconFilter;
+use App\InputFilter\AppIconPathFilter;
+
 use DomainException;
+
 use Zend\Filter\StringTrim;
 use Zend\Filter\StripTags;
 use Zend\Filter\ToInt;
-use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\FileInput;
+use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\InputFilterAwareInterface;
 use Zend\InputFilter\InputFilterInterface;
 use Zend\Validator\StringLength;
@@ -18,6 +24,7 @@ class App
     public $name;
     public $url;
     public $iconPath;
+    protected $inputFilter;
 
     public function exchangeArray(array $data)
     {
@@ -25,5 +32,46 @@ class App
         $this->name = !empty($data['name']) ? $data['name'] : null;
         $this->url = !empty($data['url']) ? $data['url'] : null;
         $this->iconPath = !empty($data['iconPath']) ? $data['iconPath'] : null;
+
+        return $this;
+    }
+
+    public function getInputFilter($hasPath = false)
+    {
+        // Uses tmpFilter Variable for if setInputFilter is ever allowed
+
+        $tmpFilter = $this->inputFilter;
+
+        if (! $this->inputFilter)
+        {
+            $tmpFilter = (new InputFilter())
+                ->merge((new AppNameFilter())->init())
+                ->merge((new AppURLFilter())->init())
+                ->merge((new AppIconFilter())->init());
+        }
+
+        if (($hasPath) && (! $tmpFilter->has('iconPath')))
+        {
+            $tmpFilter->merge((new AppIconPathFilter())->init());
+        }
+        else if ((! $hasPath) && ($tmpFilter->has('iconPath')))
+        {
+            $tmpFilter->remove("iconPath");
+        }
+
+        if (! $this->inputFilter)
+        {
+            $this->inputFilter = $tmpFilter;
+        }
+
+        return $tmpFilter;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(sprintf(
+            '%s does not allow injection of an alternate input filter',
+            __CLASS__
+        ));
     }
 }
