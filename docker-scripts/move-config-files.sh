e152487ea62532e6f58183a5d2341da5ba14ed3b
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-29 10:53:00 -0600

    Adds .env generation
    If .env does not exists, generate one from .env.example

--- docker-scripts/move-config-files.sh
+++ docker-scripts/move-config-files.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-mv ./docker-scripts/data/*.conf /etc/apache2/conf-enabled/.
-mv ./docker-scripts/data/*.ini /usr/local/etc/php/conf.d/.
+
+basePath="/var/www"
+
+mv ${basePath}/docker-scripts/data/*.conf /etc/apache2/conf-enabled/.
+mv ${basePath}/docker-scripts/data/*.ini /usr/local/etc/php/conf.d/.
+
+# Checks if the .env exists. If not use .env.example
+if [ ! -f /var/www/.env ]
+then
+    echo -e "\e[31m.env does not exists. Generating one from .env.example.\e[39m"
+    cp ${basePath}/.env.example ${basePath}/.env
+fi
