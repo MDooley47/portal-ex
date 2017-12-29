1424499f52fd1a3aa1223afa5d8398837ea6f8c9
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-29 10:54:23 -0600

    Spelling correction

--- docker-scripts/move-config-files.sh
+++ docker-scripts/move-config-files.sh
@@ -8,6 +8,6 @@ mv ${basePath}/docker-scripts/data/*.ini /usr/local/etc/php/conf.d/.
 # Checks if the .env exists. If not use .env.example
 if [ ! -f /var/www/.env ]
 then
-    echo -e "\e[31m.env does not exists. Generating one from .env.example.\e[39m"
+    echo -e "\e[31m.env does not exist. Generating one from .env.example.\e[39m"
     cp ${basePath}/.env.example ${basePath}/.env
 fi
