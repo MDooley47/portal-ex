dd3debf551c2b8e5b5e18a02914cf9acc162d949
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-29 15:17:01 -0600

    Splits table schemas into seprate files.

--- data/load_db.php
+++ data/load_db.php
@@ -1,22 +1,31 @@
 <?php
 
-try
+$tables = [
+    "apps",
+    "users",
+];
+
+$db = new PDO('pgsql:host=db', 'postgres', 'asdfgh');
+
+$db->exec("CREATE DATABASE portal;");
+
+foreach ($tables as $table)
 {
-    $db = new PDO('pgsql:host=db', 'postgres', 'asdfgh');
-
-    $db->exec("CREATE DATABASE portal;");
-
     $db = new PDO('pgsql:host=db;dbname=portal', 'postgres', 'asdfgh');
 
-    $fh = fopen(__DIR__ . '/schema.sql', 'r');
-    while ($line = fread($fh, 4096))
+    try
     {
-        $db->exec($line);
+
+        $fh = fopen(__DIR__ . "/{$table}Schema.sql", 'r');
+        while ($line = fread($fh, 4096))
+        {
+            $db->exec($line);
+        }
+        fclose($fh);
+        echo "Loaded {$table}Schema.sql\n";
     }
-    fclose($fh);
-    echo "Successfully loaded schema.sql\n";
-}
-catch (PDOException $e)
-{
-    die("DB ERROR: ". $e->getMessage() . "\n");
+    catch (PDOException $e)
+    {
+        die("DB ERROR: ". $e->getMessage() . "\n");
+    }
 }
---
+++ data/usersSchema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (id SERIAL PRIMARY KEY, slug text NOT NULL UNIQUE, name text NOT NULL, email text NOT NULL UNIQUE);
+
+
+INSERT INTO users (slug, name, email) VALUES ('njwdq3', 'Eddard Stark', 'eddard@stark.got');
+INSERT INTO users (slug, name, email) VALUES ('un9f2e', 'Catelyn Stark', 'catelyn@stark.got');
+INSERT INTO users (slug, name, email) VALUES ('3ehu9Q', 'Robb Stark', 'robb@stark.got');
+INSERT INTO users (slug, name, email) VALUES ('uhf83W', 'John Snow', 'john@snow.got');
+INSERT INTO users (slug, name, email) VALUES ('9ddsoj', 'Sansa Stark', 'sansa@stark.got');
+INSERT INTO users (slug, name, email) VALUES ('huwei2', 'Arya Stark', 'arya@stark.got');
+INSERT INTO users (slug, name, email) VALUES ('DSAF23', 'Bran Stark', 'bran@stark.got');
+INSERT INTO users (slug, name, email) VALUES ('8u2fSA', 'Rickon Stark', 'rickon@stark.got');
