ee9c6e8db5317c0afe0553cdcc130ddee9367d4f
Matthew Dooley <Matthew@Dooley.xyz>
2018-01-23 14:40:46 -0600

    Starts tracking local.php
    This is due to the fact that sensitive information is kept in dotenv.

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -3,12 +3,13 @@
  * Local Configuration Override
  *
  * This configuration override file is for overriding environment-specific and
- * security-sensitive configuration information. Copy this file without the
- * .dist extension at the end and populate values as needed.
+ * security-sensitive configuration information.
  *
  * @NOTE: This file is ignored from Git by default with the .gitignore included
  * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
  * credentials from accidentally being committed into version control.
+ * @NOTE Since dotenv is used, this file will not contain any
+ * sensitive information; therefore, it is not ingored by Git.
  */
 
 return [
