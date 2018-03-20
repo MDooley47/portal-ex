95eb1f737653b5cbe3a51c85f9644b955701c542
Matthew Dooley <Matthew@Dooley.xyz>
2018-03-20 11:46:39 -0500

    Unifies view manager

--- module/Attribute/config/module.config.php
+++ module/Attribute/config/module.config.php
@@ -2,32 +2,6 @@
 
 namespace Attribute;
 
-use Zend\Router\Http\Segment;
-use Zend\ServiceManager\Factory\InvokableFactory;
+include APPLICATION_PATH . '/module/Traits/config/returnViewManager.php';
 
-return [
-    'router' => [
-        'routes' => [
-            'attribute' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route' => '/attribute[/:action[/:slug]][/]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'slug'     => '[a-zA-Z0-9]+',
-                    ],
-                    'defaults' => [
-                        'controller' => Controller\AttributeController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
-        ],
-    ],
-
-    'view_manager' => [
-        'template_path_stack' => [
-            'attribute' => __DIR__ . '/../view',
-        ],
-    ],
-];
+return $return_view_manager;
