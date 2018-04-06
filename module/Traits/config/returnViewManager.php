85e6f2ba4cc676c4004c3a0d7fbeb67459342203
Matthew Dooley <Matthew@Dooley.xyz>
2018-04-06 13:23:07 -0500

    Moves path to a variable

--- module/Traits/config/returnViewManager.php
+++ module/Traits/config/returnViewManager.php
@@ -1,13 +1,20 @@
 <?php
 
+$viewPath = APPLICATION_PATH . '/module/Application/view';
+
 $return_view_manager = [
     'view_manager' => [
         'template_path_stack' => [
-            'app' => APPLICATION_PATH . '/module/Application/view',
-            'attribute' => APPLICATION_PATH . '/module/Application/view',
-            'group' => APPLICATION_PATH . '/module/Application/view',
-            'grouptype' => APPLICATION_PATH . '/module/Application/view',
-            'ipaddress' => APPLICATION_PATH . '/module/Application/view',
+            'app' => $viewPath,
+            'attribute' => $viewPath,
+            'group' => $viewPath,
+            'grouptype' => $viewPath,
+            'ipaddress' => $viewPath,
+            'ownertype' => $viewPath,
+            'privilege' => $viewPath,
+            'setting' => $viewPath,
+            'tab' => $viewPath,
+            'user' => $viewPath,
         ],
     ],
 ];
