43a81442b24ee49049159de533c4ea341b0fca86
Matthew Dooley <Matthew@Dooley.xyz>
2018-05-08 16:09:21 -0500

    Fixes comment

--- data/schemas/privileges_schema.sql
+++ data/schemas/privileges_schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE privileges (slug TEXT PRIMARY KEY, name TEXT NOT NULL, description TEXT);
+CREATE TABLE privileges (slug TEXT PRIMARY KEY, name TEXT NOT NULL, level INT NOT NULL, description TEXT);
 
 
-INSERT INTO privileges (slug, name, description) VALUES ('anon', 'anonymous', 'Not logged in.');
-INSERT INTO privileges (slug, name, description) VALUES ('auth', 'authenticated user', 'No special privilege.');
-INSERT INTO privileges (slug, name, description) VALUES ('admin', 'group admin', 'Admin of group, building, or organization.');
-INSERT INTO privileges (slug, name, description) VALUES ('sudo', 'system admin', '');
+INSERT INTO privileges (slug, name, level, description) VALUES ('anon', 'anonymous', 0, 'Not logged in.');
+INSERT INTO privileges (slug, name, level, description) VALUES ('auth', 'authenticated user', 10, 'No special privilege.');
+INSERT INTO privileges (slug, name, level, description) VALUES ('admin', 'group admin', 20, 'Admin of group, building, or organization.');
+INSERT INTO privileges (slug, name, level, description) VALUES ('sudo', 'system admin', 30, 'Admin of everything');
