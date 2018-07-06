5db208e00ba550c4f4c28d6a382b2c64807c14f3
Matthew Dooley <Matthew@Dooley.xyz>
2018-07-06 11:00:08 -0500

    Add support for codist seeding.

--- data/load_db.php
+++ data/load_db.php
@@ -45,3 +45,54 @@ foreach ($tables as $table)
         die("DB ERROR: ". $e->getMessage() . "\n");
     }
 }
+
+/** CODIST - CSV PROCESSING TIME **/
+
+$rows   = array_map('str_getcsv', file(__DIR__ . "/schools.csv"));
+$header = array_shift($rows);
+$csv    = array();
+foreach($rows as $row) {
+    $csv[] = array_combine($header, $row);
+}
+
+$db = new PDO('pgsql:host=db;dbname=portal', 'postgres', 'asdfgh');
+
+foreach($csv as $row)
+{
+/*
+    // Add Counties
+
+    try
+    {
+        $db->exec("INSERT INTO groups (\"groupType\", slug, name) VALUES ('j25lhv', '" . explode("-", $row["CODIST"])[0] . "', '" . ucwords(strtolower($row["COUNTY"])) . " County');");
+    }
+    catch (PDOException $e)
+    {
+        die("DB ERROR: ". $e->getMessage() . "\n");
+    }
+*/
+
+    // Add Orgranization (schools)
+
+    try
+    {
+        $db->exec("INSERT INTO groups (\"groupType\", slug, name) VALUES ('ri12io', '" . substr($row["CODIST"], 0, 7) . "', '" . ucwords(strtolower($row["SCHOOL NAME"])) . "');");
+    }
+    catch (PDOException $e)
+    {
+        die("DB ERROR: ". $e->getMessage() . "\n");
+    }
+
+/*
+    // Add Buildings
+
+    try
+    {
+        $db->exec("INSERT INTO groups (\"groupType\", slug, name) VALUES ('ri12io', '" . $row["CODIST"] . "', '" . ucwords(strtolower($row["SCHOOL NAME"])) . "');");
+    }
+    catch (PDOException $e)
+    {
+        die("DB ERROR: ". $e->getMessage() . "\n");
+    }
+*/
+}
