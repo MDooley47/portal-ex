c027b3cefb58d0b7b67b642a1292daaf7d354d95
Matthew Dooley <Matthew@Dooley.xyz>
2018-07-23 17:29:40 -0500

    Adds CorrelationInterface

---
+++ module/Traits/Interfaces/CorrelationInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Traits\Interfaces;
+
+interface CorrelationInterface
+{
+    public function correlationExists($element1, $element2, $options);
+    public function addCorrelation($element1, $element2, $options);
+}
