1a526e0437d7174d8fd8567091ae842d70abe16c
Matthew Dooley <Matthew@Dooley.xyz>
2018-09-04 19:29:25 +0000

    Apply fixes from StyleCI

--- module/Traits/partials/signin-out.php
+++ module/Traits/partials/signin-out.php
@@ -1,9 +1,9 @@
 <?php
+
 use SessionManager\Session;
 
 if (Session::isActive()) {
-    print "<a href=" . $this->url('logout') . " class=\"post-noslug\">Sign Out</a>";
+    echo '<a href='.$this->url('logout').' class="post-noslug">Sign Out</a>';
 } else {
-    print "<a href = " . $this->url('login') . " > Sign In </a >";
+    echo '<a href = '.$this->url('login').' > Sign In </a >';
 }
-?>
