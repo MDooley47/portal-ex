f29c8931ddc824a6cc6d04cb29522f91c3548b8f
Matthew Dooley <Matthew@Dooley.xyz>
2018-09-04 14:28:17 -0500

    Update style

--- module/Traits/partials/signin-out.php
+++ module/Traits/partials/signin-out.php
@@ -1,8 +1,9 @@
 <?php
 use SessionManager\Session;
 
-if (Session::isActive()) : ?>
-<a href="<?= $this->url('logout') ?>" class="post-noslug">Sign Out</a>
-<? else : ?>
-<a href="<?= $this->url('login') ?>">Sign In</a>
-<? endif; ?>
+if (Session::isActive()) {
+    print "<a href=" . $this->url('logout') . " class=\"post-noslug\">Sign Out</a>";
+} else {
+    print "<a href = " . $this->url('login') . " > Sign In </a >";
+}
+?>
