cd02061a3232871a7f5e2f6d1e043833b1855bdf
Scott Isaacson <sisaacson@esucc.org>
2018-11-08 13:29:20 -0600

    Improve error reporting when no group or tab is defined for a user.

--- module/Application/src/Controller/ApplicationController.php
+++ module/Application/src/Controller/ApplicationController.php
@@ -35,7 +35,31 @@ class ApplicationController extends AbstractActionController
             Session::hasPrivilege('auth');
 
             $user = Session::getUser();
-            $tab = $user->defaultTab();
+            if ($user)
+            {
+              $tab = $user->defaultTab();
+              if ($tab)
+              {
+                $apps = $tab->getApps();
+              }
+              else
+              {
+                $portalError = true;
+                $portalErrorMessage = 'No applications could be located for you. Please contact your technology support staff.';
+              }
+            }
+            else
+            {
+              $portalError = true;
+              $portalErrorMessage = 'We cannot find your user profile. Please contact your technology support staff.';
+            }
+            if ($portalError)
+            {
+              return (new ViewModel([
+                'portalError' => $portalError,
+                'portalErrorMessage' => $portalErrorMessage,
+              ]));
+            }
             return (new ViewModel([
                 'apps' => $tab->getApps(),
             ]))
@@ -43,7 +67,7 @@ class ApplicationController extends AbstractActionController
         }
     }
 
-    public function loginAction()
+    public function loginoldAction()
     {
         if ($this->getRequest()->isPost()) {
             return $this->loginPostAction();
@@ -56,7 +80,7 @@ class ApplicationController extends AbstractActionController
         return new ViewModel();
     }
 
-    public function loginssoAction()
+    public function loginAction()
     {
         // initiates SAML SSO login using SimpleSAMLphp SP
         //   7/16/2018 SI
