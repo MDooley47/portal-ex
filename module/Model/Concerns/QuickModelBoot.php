bfe5a661838f4a5aefdd5b49ea0614b713dbdeee
Matthew Dooley <Matthew@Dooley.xyz>
2019-11-06 20:30:40 -0500

    Removes parent variable setting in QuickModelBoot

--- module/Model/Concerns/QuickModelBoot.php
+++ module/Model/Concerns/QuickModelBoot.php
@@ -6,7 +6,6 @@ trait QuickModelBoot
 {
     public static function boot()
     {
-        parent::$table = self::$table;
-        parent::$primaryKey = self::$primaryKey ?? parent::$primaryKey;
+
     }
 }
