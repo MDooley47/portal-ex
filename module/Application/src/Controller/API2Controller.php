af93fd21b598567f791d1330ac7fc0d90beabe06
Matthew Dooley <Matthew@Dooley.xyz>
2019-11-06 20:32:32 -0500

    Adds getVerb support to API2Controller

--- module/Application/src/Controller/API2Controller.php
+++ module/Application/src/Controller/API2Controller.php
@@ -8,7 +8,10 @@
 
 namespace Application\Controller;
 
+use SessionManager\Session;
 use Traits\HasTables;
+use Zend\Http\Headers;
+use Zend\Http\Response;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class API2Controller extends AbstractActionController
@@ -24,6 +27,13 @@ class API2Controller extends AbstractActionController
     {
         $this->makeParams();
         $this->verb = $this->getRequest()->getMethod();
+
+        Session::start();
+
+        $this->response = new Response();
+        $this->headers = new Headers();
+        $this->headers->addHeaderLine('Content-Type', 'text/json');
+        $this->response->setHeaders($this->headers);
     }
 
     public function makeParams()
@@ -47,49 +57,75 @@ class API2Controller extends AbstractActionController
     {
         $this->init();
 
-        $this->handleVerb();
+        $content = $this->handleVerb();
 
-        die();
+        $response = $this->response->setContent($content);
+
+        return $response;
     }
 
     public function handleVerb()
     {
+        $content = null;
+
         switch ($this->verb) {
             case 'DELETE':
-                $this->deleteVerb();
+                $content = $this->deleteVerb();
                 break;
             case 'GET':
-                $this->getVerb();
+                $content = $this->getVerb();
                 break;
             case 'PATCH':
-                $this->patchVerb();
+                $content = $this->patchVerb();
                 break;
             case 'POST':
-                $this->postVerb();
+                $content = $this->postVerb();
                 break;
             case 'PUT':
-                $this->putVerb();
+                $content = $this->putVerb();
                 break;
             default:
                 die('ERROR UNKNOWN HTTP REQUEST METHOD');
         }
+
+        return json_encode($content);
+    }
+
+    public function resolveModelSlug($model, $slug)
+    {
+        $model = resolveModel(singularize($model));
+        return is_array($output = $model::find($slug)) ? $output : $output->getArrayCopy();
     }
 
     public function deleteVerb()
     {
+
     }
 
     public function getVerb()
     {
+        $outputs = [];
         foreach ($this->parameters as $key => $param) {
-            $this->getHelper($key, $param);
+            if (   (($key == 'users')      &&
+                        (!(Session::hasPrivilege('sudo') || $param === Session::get('userSlug')))
+                   )
+                || (($key == 'groups')     && !Session::hasPrivilege('auth', $param))
+                || (($key == 'privileges') && !Session::hasPrivilege('sudo', $key))
+            ) continue;
+            else if (empty($param)) {
+                # TODO: all must support a method to check Privilege
+                # && Session::hasPrivilege('sudo')
+                $resolvedModel = resolveModel(singularize($key));
+                $outputs[$key] = [];
+                foreach ($resolvedModel::all() as $model) {
+                    array_push($outputs[$key], $model->getArrayCopy());
+                }
+            } else {
+                $requestedModel = $this->resolveModelSlug($key, $param);
+                $outputs[$key] = $requestedModel;
+            }
         }
-    }
-
-    public function getHelper($model, $slug)
-    {
-        $model = resolveModel(singularize($model));
-        dd($model::all());
+        return $outputs;
     }
 
     public function patchVerb()
